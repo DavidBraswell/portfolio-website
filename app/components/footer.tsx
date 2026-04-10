4a3b6da056f9bbcab88b59956d6be29b285cd616
kiffer <228941467+DavidBraswell@users.noreply.github.com>
2026-04-10 01:28:40 -0500

    add blog entry

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,18 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://github.com/davidbraswell"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -49,7 +38,7 @@ export default function Footer() {
             href="https://vercel.com/templates/next.js/portfolio-starter-kit"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">linkedin</p>
           </a>
         </li>
       </ul>
