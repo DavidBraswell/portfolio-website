58a01aac894dad4971fa4152a8d21d1cd18e3f31
kiffer <228941467+DavidBraswell@users.noreply.github.com>
2026-04-10 00:47:32 -0500

    remove deploy

---
+++ app/assets/projects.ts
@@ -0,0 +1,23 @@
+export const PROJECTS = [
+  {
+    name: "George Eliot Archive Social Network Visualization",
+    images: ["social-network.png"],
+    description:
+      "The George Eliot Archive is an academic literary research project housing all of George Eliot's written works. Users can access George Eliot's relationship web, which visualizes every known relationship between everyone she knew and interacted with.",
+    summary:
+      "Overhauled a data visualization JavaScript applet for the George Eliot Archive, implementing real-time updates, optimizing algorithms for 30% faster performance, and redesigning the UI/UX to improve accuracy, usability, and researcher engagement.",
+    link: "",
+    tags: ["Javascript", "D3.js", "Node.js"],
+  },
+  {
+    name: "Not-ify",
+    images: ["staff-website.png"],
+    description:
+      "A full-stack clone of the popular app Spotify.",
+    summary:
+      "Redesigned and redeveloped Auburn University's staff website with a modern, responsive Next.js and Tailwind CSS UI, improving accessibility, performance, and engagement. Leveraged server-side rendering, caching strategies, and CI/CD deployment on Vercel for fast, seamless updates.",
+    link: "",
+    tags: ["React", "Next.js", "Tailwind CSS", "Vercel"],
+  },
+
+];
