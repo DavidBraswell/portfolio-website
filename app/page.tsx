a98174c456c2372c7d6eae5224bf8c7b82d06318
David Braswell <228941467+DavidBraswell@users.noreply.github.com>
2026-04-12 02:58:14 -0500

    updated bio

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,8 @@ export default function Page() {
       <p className="mb-4">
         {`Hi, I'm David! I'm a graduate in computer science from Auburn University with a solid foundation in C++, 
         Node.js, HTML/CSS, and Express.js. I specialize in building interactive and accessible web applications. I'm
-        also interested in game dev, and tinker with passion projects in my free time.`}
+        also interested in game dev, and tinker with passion projects in my free time. I hope to make up for where I lack in formal experience
+        with the projects I've worked on and experience I gained in the classroom.`}
       </p>
 
     </section>
