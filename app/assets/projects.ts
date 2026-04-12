923552f1ce85c6d0cc8c480e95e31f45213361ca
David Braswell <228941467+DavidBraswell@users.noreply.github.com>
2026-04-12 02:10:33 -0500

    updated projects section, added images

--- app/assets/projects.ts
+++ app/assets/projects.ts
@@ -7,7 +7,14 @@ export const PROJECTS = [
     summary:
       "Overhauled a data visualization JavaScript applet for the George Eliot Archive, implementing real-time updates, optimizing algorithms for 30% faster performance, and redesigning the UI/UX to improve accuracy, usability, and researcher engagement.",
     link: "",
-    tags: ["Javascript", "D3.js", "Node.js"],
+    tags: ["JavaScript", "D3.js", "Node.js"],
+  },
+  {
+    name: "CSFloat Bargain Bot",
+    images: ["screen1.png"],
+    description: "CSFloat is a platform where users list in game items for real life currency. This bot automatically makes offers on new listings at fair prices with the intention of reselling the item for a project margin of 4-6%.",
+    summary: "This bot tested my knowledge of API calls, asynchronous JavaScript programming, and CSS",
+    tags: ["JavaScript", "CSS", "Axios", "Express.js"]
   },
   {
     name: "Auburn University Staff Website Redesign",
