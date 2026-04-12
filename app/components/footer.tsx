a75452a78f61caf6ba11c98ca004ab49209b65a8
David Braswell <228941467+DavidBraswell@users.noreply.github.com>
2026-04-12 02:57:50 -0500

    updated footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://www.linkedin.com/in/david-braswell/"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">linkedin</p>
