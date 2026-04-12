97e124572429f172b2c7a702ca1602b384c9486f
David Braswell <228941467+DavidBraswell@users.noreply.github.com>
2026-04-12 02:18:35 -0500

    updated projects list

--- app/assets/projects.ts
+++ app/assets/projects.ts
@@ -6,7 +6,7 @@ export const PROJECTS = [
       "The George Eliot Archive is an academic literary research project housing all of George Eliot's written works. Users can access George Eliot's relationship web, which visualizes every known relationship between everyone she knew and interacted with.",
     summary:
       "Overhauled a data visualization JavaScript applet for the George Eliot Archive, implementing real-time updates, optimizing algorithms for 30% faster performance, and redesigning the UI/UX to improve accuracy, usability, and researcher engagement.",
-    link: "",
+    link: "https://georgeeliotarchive.org/relationships",
     tags: ["JavaScript", "D3.js", "Node.js"],
   },
   {
@@ -14,6 +14,7 @@ export const PROJECTS = [
     images: ["screen1.png"],
     description: "CSFloat is a platform where users list in game items for real life currency. This bot automatically makes offers on new listings at fair prices with the intention of reselling the item for a project margin of 4-6%.",
     summary: "This bot tested my knowledge of API calls, asynchronous JavaScript programming, and CSS",
+    link: "",
     tags: ["JavaScript", "CSS", "Axios", "Express.js"]
   },
   {
@@ -25,6 +26,5 @@ export const PROJECTS = [
       "Redesigned and redeveloped Auburn University's staff website with a modern, responsive Next.js and Tailwind CSS UI, improving accessibility, performance, and engagement. Leveraged server-side rendering, caching strategies, and CI/CD deployment on Vercel for fast, seamless updates.",
     link: "",
     tags: ["React", "Next.js", "Tailwind CSS", "Vercel"],
-  },
-
+  }
 ];
