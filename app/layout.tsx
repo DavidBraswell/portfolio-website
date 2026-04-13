ab75eda26b0fb943f142f887e5ed2fa9004e83c4
David Braswell <228941467+DavidBraswell@users.noreply.github.com>
2026-04-13 01:55:01 -0500

    double error

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
-import { Analytics } from '@vercel/analytics/next';
+
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://portfolio-blog-starter.vercel.app"),
