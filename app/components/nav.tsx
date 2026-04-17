ac305ed7c2456b655a0a2dba227b5b5349362a5b
David Braswell <228941467+DavidBraswell@users.noreply.github.com>
2026-04-17 02:10:55 -0500

    fix resume

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -42,7 +42,7 @@ export function Navbar() {
               );
             })}
             <a
-              href="~resume/DavidBraswellResume.pdf"
+              href="DavidBraswellResume.pdf"
               className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
             >
               resume
