62949bbc94c5bd791feaabdf180117a5abe6d7a7
David Braswell <228941467+DavidBraswell@users.noreply.github.com>
2026-05-11 23:32:37 -0500

    page tweaks

--- app/assets/projects.ts
+++ app/assets/projects.ts
@@ -1,13 +1,23 @@
 export const PROJECTS = [
+    {
+    name: "Field Ops Dashboard",
+    images: [],
+    description:
+      "A full stack dashboard with useful tools for managing field operations.",
+    summary:
+      "Full stack project utilizing the PERN (Postgres, Express, React, Node.js) stack for hands on experience with Postgres and relational databases. I wanted to create a tool that could reasonably be used by a company to assist with field work. Detailed info for how I deployed it is availabe on the GitHub Repository.",
+    link: "https://github.com/DavidBraswell/field-ops-dashboard",
+    tags: ["React", "Vite", "Postgres", "Express.js"],
+  },
   {
     name: "George Eliot Archive Social Network Visualization",
     images: ["social-network.png"],
     description:
       "The George Eliot Archive is an academic literary research project housing all of George Eliot's written works. Users can access George Eliot's relationship web, which visualizes every known relationship between everyone she knew and interacted with.",
     summary:
-      "Overhauled a data visualization JavaScript applet for the George Eliot Archive, implementing real-time updates, optimizing algorithms for 30% faster performance, and redesigning the UI/UX to improve accuracy, usability, and researcher engagement.",
+      "Overhauled a data visualization JavaScript applet for the George Eliot Archive, implementing real-time updates, optimizing algorithms for faster performance, and redesigning the UI/UX to improve accuracy, usability, and researcher engagement.",
     link: "https://georgeeliotarchive.org/relationships",
-    tags: ["JavaScript", "D3.js", "Node.js"],
+    tags: ["Node.js", "D3.js", "Tailwind"],
   },
   {
     name: "CSFloat Bargain Bot",
@@ -15,16 +25,7 @@ export const PROJECTS = [
     description: "CSFloat is a platform where users list in game items for real life currency. This bot automatically makes offers on new listings at fair prices with the intention of reselling the item for a profit margin of 4-6%.",
     summary: "The user can configure a number of settings and rules for the bot to follow, and it automatically scans new listings and sends offers to those matching the defined criteria.",
     link: "",
-    tags: ["Express.js", "Axios", "Node.js", "CSS"]
-  },
-  {
-    name: "Field Equipment Tracker",
-    images: [],
-    description:
-      "A dashboard for managing inventory. Users can add, delete, and assign equipment to job site locations.",
-    summary:
-      "Full stack project utilizing the PERN (Postgres, Express, React, Node.js) stack for hands on experience with Postgres and relational databases. I wanted to create a tool that could reasonably be used by a company to keep track of their equipment.",
-    link: "https://github.com/DavidBraswell/field-equipment-tracker",
-    tags: ["React", "Vite", "Tailwind CSS", "Postgres"],
+    tags: ["Express.js", "Node.js", "CSS"]
   }
+
 ];
